85b12a35a3ccc67db618e8cf57ccc38805618285
shunmugapriya <priyakrishnan816@gmail.com>
2023-07-27 13:30:16 +0530

    password app task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [result, setResult] = useState(null);
+  function isStrongPassword(password) {
+    const hasLowercase = /[a-z]/.test(password);
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasDigit = /\d/.test(password);
+    const isWithinLengthLimits = password.length >= 6 && password.length <= 20;
+    const hasNoRepeatingChars = !/(.)\1\1/.test(password);
+    return hasLowercase && hasUppercase && hasDigit && isWithinLengthLimits && hasNoRepeatingChars;
+  }
+  function minStepsToStrongPassword(password) {
+    if (isStrongPassword(password)) {
+      return 0;
+    }
+    let missingConditions = 0;
+    if (!/[a-z]/.test(password)) {
+      missingConditions++;
+    }
+    if (!/[A-Z]/.test(password)) {
+      missingConditions++;
+    }
+    if (!/\d/.test(password)) {
+      missingConditions++;
+    }
+    if (password.length < 6) {
+      missingConditions += 6 - password.length;
+    } else if (password.length > 20) {
+      missingConditions += password.length - 20;
+    }
+    const repeatingCharsRegex = /(.)\1\1/g;
+    let repeatingCharsCount = 0;
+    let match;
+    while ((match = repeatingCharsRegex.exec(password)) !== null) {
+      repeatingCharsCount++;
+    }
+    missingConditions = Math.max(missingConditions, repeatingCharsCount);
+    return missingConditions;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const minSteps = minStepsToStrongPassword(password);
+    setResult(minSteps);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Password Strength Program</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="passwordInput">Enter password:</label>
+            <input
+              type="password"
+              id="passwordInput"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Calculate</button>
+        </form>
+        {result !== null && (
+          <div className="result">
+            <h2>Result:</h2>
+            <p>{`Minimum number of steps required: ${result}`}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
